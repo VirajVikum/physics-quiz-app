3e03f9b2a475086733257c6918311346c1be76c6
Viraj Vikum <virajempire2001@gmail.com>
2024-10-18 15:15:31 +0530

    developed the client front page to show question categories

--- app/Livewire/Client/Questions/Index.php
+++ app/Livewire/Client/Questions/Index.php
@@ -2,18 +2,25 @@
 
 namespace App\Livewire\Client\Questions;
 
+use App\Models\Question;
 use Livewire\Component;
 
 class Index extends Component
 {
     public $sub_category;
     public $category;
+    public $questions;
 
     public function mount($cat,$sub)
     {
         $this->sub_category=$sub;
         $this->category=$cat;
+
+        $this->questions = Question::where('category', $this->category)
+                                ->where('sub_category', $this->sub_category)
+                                ->get();
     }
+
 
     public function render()
     {
