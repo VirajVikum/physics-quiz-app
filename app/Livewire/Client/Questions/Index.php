62ddfecd56329569222b21c882745daa37d92ec5
Viraj Vikum <virajempire2001@gmail.com>
2024-10-24 15:24:30 +0530

    updated the questions interfaces

--- app/Livewire/Client/Questions/Index.php
+++ app/Livewire/Client/Questions/Index.php
@@ -10,17 +10,22 @@ class Index extends Component
     public $sub_category;
     public $category;
     public $questions;
+    public $selectedOption;
 
-    public function mount($cat,$sub)
+    public function mount($cat, $sub)
     {
-        $this->sub_category=$sub;
-        $this->category=$cat;
+        $this->sub_category = $sub;
+        $this->category = $cat;
 
         $this->questions = Question::where('category', $this->category)
-                                ->where('sub_category', $this->sub_category)
-                                ->get();
+            ->where('sub_category', $this->sub_category)
+            ->get();
     }
 
+    public function optionSelected($option)
+    {
+        $this->selectedOption = $option;
+    }
 
     public function render()
     {
--- resources/views/livewire/client/questions/index.blade.php
+++ resources/views/livewire/client/questions/index.blade.php
@@ -15,17 +15,24 @@
                     
 
                     @foreach($questions as $question)
-       <h1>{{ $loop->iteration }}. {{ $question->question }}</h1> <!-- Question number -->
+                    <div class=" bg-blue-100 m-2 rounded-md p-2">
+       <h1 class=" font-bold md:text-xl sm:text-sm lg:text-xl">{{ $loop->iteration }}. {{ $question->question }}</h1> <!-- Question number -->
 
        <!-- Radio buttons with text on the right -->
-       <label><input type="radio" name="question_{{ $question->id }}" value="{{$question->option1}}"> {{$question->option1}}</label><br>
+       <button wire:click="optionSelected('{{$question->option1}}')" class="border border-lightBlue hover:border-blue-400 rounded mb-4 ml-10 mt-4 p-1 @if($selectedOption == $question->answer) bg-green-400 @endif"> i.) {{$question->option1}}</button> <br>
+       <button class="border border-lightBlue hover:border-blue-400 rounded mb-4 ml-10 p-1 @if($selectedOption === 'option3') bg-green-400 @endif"> ii.) {{$question->option2}}</button><br>
+       <button class="border border-lightBlue hover:border-blue-400 rounded mb-4 ml-10 p-1 @if($selectedOption === 'option3') bg-green-400 @endif"> iii.) {{$question->option3}}</button><br>
+       <button class="border border-lightBlue hover:border-blue-400 rounded mb-4 ml-10 p-1 @if($selectedOption === 'option3') bg-green-400 @endif"> iv.) {{$question->option4}}</button><br>
+       <button class="border border-lightBlue hover:border-blue-400 rounded mb-4 ml-10 p-1 @if($selectedOption === 'option3') bg-green-400 @endif">  v.) {{$question->option5}}</button><br>
+       {{-- <label><input type="radio" name="question_{{ $question->id }}" value="{{$question->option1}}"> {{$question->option1}}</label><br>
        <label><input type="radio" name="question_{{ $question->id }}" value="{{$question->option2}}"> {{$question->option2}}</label><br>
        <label><input type="radio" name="question_{{ $question->id }}" value="{{$question->option3}}"> {{$question->option3}}</label><br>
        <label><input type="radio" name="question_{{ $question->id }}" value="{{$question->option4}}"> {{$question->option4}}</label><br>
-       <label><input type="radio" name="question_{{ $question->id }}" value="{{$question->option5}}"> {{$question->option5}}</label><br>
+       <label><input type="radio" name="question_{{ $question->id }}" value="{{$question->option5}}"> {{$question->option5}}</label><br> --}}
        
        <!-- Break between questions -->
        <br>
+    </div>
     @endforeach
                 </div>
             </div>
