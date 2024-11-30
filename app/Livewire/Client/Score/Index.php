df569962944123e46b95918d0424ca666d8453be
Viraj Vikum <virajempire2001@gmail.com>
2024-12-01 01:41:13 +0530

    Built the logic for achievement badges

--- app/Livewire/Client/Score/Index.php
+++ app/Livewire/Client/Score/Index.php
@@ -2,10 +2,31 @@
 
 namespace App\Livewire\Client\Score;
 
+use App\Models\ClientAchievement;
+use App\Models\QuestionCategory;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $completedPercentage;
+
+    public function mount()
+    {
+    $totalSubCategories = QuestionCategory::where('delete_status', 0)
+    ->get()
+    ->reduce(function ($carry, $item) {
+        // Split sub_categories by commas and count them
+        $subCategoryCount = count(explode(',', $item->sub_categories));
+        return $carry + $subCategoryCount;
+    }, 0);
+
+    $completedCount = ClientAchievement::where('client_id',auth()->id())
+    ->count();
+
+    $this->completedPercentage = floor(($completedCount / $totalSubCategories) * 100);
+
+}
+
     public function render()
     {
         return view('livewire.client.score.index')->layout('layouts.app');
