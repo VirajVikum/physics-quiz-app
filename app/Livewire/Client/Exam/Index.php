9be770e71cda0c95957f35e88fad9fa9404beaf7
Viraj Vikum <virajempire2001@gmail.com>
2024-12-01 00:03:52 +0530

    updated the exam section

--- app/Livewire/Client/Exam/Index.php
+++ app/Livewire/Client/Exam/Index.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Client\Exam;
 
+use App\Models\ClientAchievement;
 use Livewire\Component;
 use App\Models\ClientActivityDetail;
 use App\Models\Question;
@@ -21,37 +22,37 @@ class Index extends Component
     public $questionCount = 0;
     public $errorMessage = null;
     public $result;
-    public $showResult =false;
+    public $showResult = false;
+    public $grade;
 
-    public $MaxQuizResult =0;
+    public $MaxQuizResult = 0;
 
-    
 
-    public function mount($cat,$lvl , $sub)
+
+    public function mount($cat, $lvl, $sub)
     {
         $this->sub_category = $sub;
         $this->category = $cat;
-        $this->level= $lvl;
+        $this->level = $lvl;
 
         $this->questions = Question::where('category', $this->category)
-        ->where('type', 'Quiz')
-        ->where('level', $this->level)
-        ->where('sub_category', $this->sub_category)
-        ->inRandomOrder()
-        ->take(20)
-        ->get();
+            ->where('type', 'Exam')
+            ->where('level', $this->level)
+            ->where('sub_category', $this->sub_category)
+            ->inRandomOrder()
+            ->take(20)
+            ->get();
 
-        $maxMark = ClientActivityDetail::where('category', $this->category)
-        ->where('client_id',auth()->id())
-        ->where('type', 'Quiz')
-        ->where('level', $this->level)
-        ->where('sub_category', $this->sub_category)
-        ->max('marks');
+        $maxMark = ClientAchievement::where('category', $this->category)
+            ->where('client_id', auth()->id())
+            ->where('level', $this->level)
+            ->where('sub_category', $this->sub_category)
+            ->max('marks');
 
         $this->MaxQuizResult = $maxMark;
     }
 
-    public function optionSelected($questionId,$option)
+    public function optionSelected($questionId, $option)
     {
         $this->selectedQuestionId = $questionId;
         $this->selectedOption = $option;
@@ -59,7 +60,7 @@ class Index extends Component
         $this->completedQuestions[$questionId] = true;
         $this->questionCount++;
 
-        $question = $this->questions->firstWhere('id', $questionId); 
+        $question = $this->questions->firstWhere('id', $questionId);
         if ($question && $option === $question->answer) {
             $this->correctAnswersCount++;
         }
@@ -70,21 +71,63 @@ class Index extends Component
         if ($this->questionCount == 20) {
             // Proceed with showing results
             $this->errorMessage = null; // Clear any previous error
-            $this->result=($this->correctAnswersCount/ $this->questionCount)*100;
-            $this->showResult=true;
+            $this->result = ($this->correctAnswersCount / $this->questionCount) * 100;
+            $this->showResult = true;
 
             $data = [
                 'client_id' => auth()->id(),
-                'type' => 'Quiz',
+                'type' => 'Exam',
                 'level' => $this->level,
                 'category' => $this->category,
                 'sub_category' => $this->sub_category,
                 'marks' => $this->result,
             ];
-    
-            // Create the record
+
             $activityDetail = ClientActivityDetail::create($data);
-          
+
+            $marks = $this->result;
+
+            if ($marks >= 75) {
+                $this->grade = 'A';
+            } elseif ($marks >= 65) {
+                $this->grade = 'B';
+            } elseif ($marks >= 40) {
+                $this->grade = 'C';
+            }
+
+            $achievement = [
+                'client_id' => auth()->id(),
+                'level' => $this->level,
+                'category' => $this->category,
+                'sub_category' => $this->sub_category,
+                'marks' => $this->result,
+                'grade' => $this->grade,
+            ];
+
+            // Create the record
+
+            $existingRecord = ClientAchievement::where('client_id', auth()->id())
+                ->where('level', $this->level)
+                ->where('category', $this->category)
+                ->where('sub_category', $this->sub_category)
+                ->first();
+
+                if ($marks >= 40) {
+                    if ($existingRecord) {
+                        // Update the existing record
+                        if($this->MaxQuizResult < $marks)
+                        {
+                        $existingRecord->update([
+                            'marks' => $marks,
+                            'grade' => $this->grade,
+                        ]);
+                    }
+                    } else {
+                        $activityDetail = ClientAchievement::create($achievement);
+                    }
+                }
+
+
             // Code to show the results
         } else {
             $this->errorMessage = "Please answer all 20 questions before viewing the results.";
